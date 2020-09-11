3cf7b3aa4b66335dffb126da3fc1c6e986a117b5
Kyle Zheng <kylezheng73@gmail.com>
2020-09-11 02:37:03 -0400

    finalize site

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ const audioPlayer = document.getElementById("audio");
 const button = document.getElementById("button");
 const main = document.getElementsByClassName("main")[0];
 
+const splide = new Splide( '.splide' , {
+  type:"loop",
+  pagination:false,
+  pauseOnHover:false,
+  pauseOnFocus:false,
+  direction:"ttb",
+  // autoplay:true,
+  arrows:false,
+  width:"100vw",
+  height:"100vh",
+  interval:500,
+}).mount();
+
 button.addEventListener("click", () => {
   button.style.visibility = "hidden";
   audioPlayer.play();
   main.classList.add("main2");
   setTimeout(()=>main.classList.add("main3"), 2000);
+  setTimeout(()=>go(),3000);
+  // splide.play();
+  // splide.options = {autoplay:true};
 });
+
+function go(){
+  setTimeout(()=>go(),3000);
+  splide.go('+1');
+}
+
+
